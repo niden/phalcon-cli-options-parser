3d77773ace1327768d75424147fe2816e7178601
Nikolaos Dimopoulos <nikos@niden.net>
2025-01-10 16:11:06 -0600

    added exceptions

---
+++ src/Exception/InvalidStreamException.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of the Cop package.
+ *
+ * (c) Phalcon Team <team@phalconphp.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Cop\Exception;
+
+use Exception;
+
+class InvalidStreamException extends Exception
+{
+}
