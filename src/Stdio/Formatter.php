148000652d7d72927a0be9aba43bd6fc75de07e0
Nikolaos Dimopoulos <nikos@niden.net>
2025-01-10 10:57:59 -0600

    added STDIO formatter and test

---
+++ src/Stdio/Formatter.php
@@ -0,0 +1,120 @@
+<?php
+
+/**
+ * This file is part of the Cop package.
+ *
+ * (c) Phalcon Team <team@phalconphp.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Cop\Stdio;
+
+/**
+ * Converts <<markup>> to VT100 display control codes for POSIX, or strips
+ * the <<markup>> entirely for non-POSIX.
+ */
+class Formatter
+{
+    /**
+     * @var array<string, string>
+     */
+    protected array $codes = [
+        '<reset>'      => '0',
+        '<strong>'     => '1',
+        '</strong>'    => '0',
+        '<ul>'         => '4',
+        '</ul>'        => '0',
+        '<em>'         => '3',
+        '</em>'        => '0',
+        '<blink>'      => '5',
+        '</blink>'     => '0',
+        '<reverse>'    => '7',
+        '</reverse>'   => '0',
+        '<black>'      => '30',
+        '</black>'     => '0',
+        '<red>'        => '31',
+        '</red>'       => '0',
+        '<green>'      => '32',
+        '</green>'     => '0',
+        '<yellow>'     => '33',
+        '</yellow>'    => '0',
+        '<blue>'       => '34',
+        '</blue>'      => '0',
+        '<magenta>'    => '35',
+        '</magenta>'   => '0',
+        '<cyan>'       => '36',
+        '</cyan>'      => '0',
+        '<white>'      => '37',
+        '</white>'     => '0',
+        '<blackbg>'    => '40',
+        '</blackbg>'   => '0',
+        '<redbg>'      => '41',
+        '</redbg>'     => '0',
+        '<greenbg>'    => '42',
+        '</greenbg>'   => '0',
+        '<yellowbg>'   => '43',
+        '</yellowbg>'  => '0',
+        '<bluebg>'     => '44',
+        '</bluebg>'    => '0',
+        '<magentabg>'  => '45',
+        '</magentabg>' => '0',
+        '<cyanbg>'     => '46',
+        '</cyanbg>'    => '0',
+        '<whitebg>'    => '47',
+        '</whitebg>'   => '0',
+    ];
+
+    /**
+     * Converts <markup> in text to VT100 control codes for POSIX, or removes
+     * them for non-POSIX.
+     *
+     * @param string $input
+     * @param bool   $isPosix
+     *
+     * @return string
+     */
+    public function format(string $input, bool $isPosix): string
+    {
+        if ($isPosix) {
+            return $this->formatForPosix($input);
+        } else {
+            return $this->stripMarkup($input);
+        }
+    }
+
+    /**
+     * Format the string for POSIX
+     *
+     * @param string $input
+     *
+     * @return string
+     */
+    protected function formatForPosix(string $input): string
+    {
+        $search  = array_keys($this->codes);
+        $replace = array_map(
+            function ($code) {
+                return chr(27) . '[' . $code . 'm';
+            },
+            $this->codes
+        );
+
+        return str_replace($search, $replace, $input);
+    }
+
+    /**
+     * Strip markup from the output
+     *
+     * @param string $input
+     *
+     * @return string
+     */
+    protected function stripMarkup(string $input): string
+    {
+        return str_replace(array_keys($this->codes), '', $input);
+    }
+}
---
+++ tests/unit/Stdio/FormatterTest.php
@@ -0,0 +1,163 @@
+<?php
+
+/**
+ * This file is part of the Cop package.
+ *
+ * (c) Phalcon Team <team@phalconphp.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Cop\Tests\Unit;
+
+use Phalcon\Cop\Stdio\Formatter;
+use PHPUnit\Framework\TestCase;
+
+class FormatterTest extends TestCase
+{
+    /**
+     * @dataProvider getExamples
+     *
+     * @param string $input
+     * @param string $expected
+     * @param bool   $posix
+     *
+     * @return void
+     */
+    public function testFormat(
+        string $input,
+        string $expected,
+        bool $posix
+    ): void {
+        $formatter = new Formatter();
+
+        $actual = $formatter->format($input, $posix);
+        $this->assertSame($expected, $actual);
+    }
+
+    public static function getExamples(): array
+    {
+        $esc = chr(27);
+
+        return [
+            [
+                '<strong>bold text</strong>',
+                "{$esc}[1mbold text{$esc}[0m",
+                true,
+            ],
+            [
+                '<strong>bold<reset> text',
+                "{$esc}[1mbold{$esc}[0m text",
+                true,
+            ],
+            [
+                '<ul>underline text</ul>',
+                "{$esc}[4munderline text{$esc}[0m",
+                true,
+            ],
+            [
+                '<em>italic text</em>',
+                "{$esc}[3mitalic text{$esc}[0m",
+                true,
+            ],
+            [
+                '<blink>blink text</blink>',
+                "{$esc}[5mblink text{$esc}[0m",
+                true,
+            ],
+            [
+                '<reverse>reverse text</reverse>',
+                "{$esc}[7mreverse text{$esc}[0m",
+                true,
+            ],
+            [
+                '<black>black text</black>',
+                "{$esc}[30mblack text{$esc}[0m",
+                true,
+            ],
+            [
+                '<red>red text</red>',
+                "{$esc}[31mred text{$esc}[0m",
+                true,
+            ],
+            [
+                '<green>green text</green>',
+                "{$esc}[32mgreen text{$esc}[0m",
+                true,
+            ],
+            [
+                '<yellow>yellow text</yellow>',
+                "{$esc}[33myellow text{$esc}[0m",
+                true,
+            ],
+            [
+                '<blue>blue text</blue>',
+                "{$esc}[34mblue text{$esc}[0m",
+                true,
+            ],
+            [
+                '<magenta>magenta text</magenta>',
+                "{$esc}[35mmagenta text{$esc}[0m",
+                true,
+            ],
+            [
+                '<cyan>cyan text</cyan>',
+                "{$esc}[36mcyan text{$esc}[0m",
+                true,
+            ],
+            [
+                '<white>white text</white>',
+                "{$esc}[37mwhite text{$esc}[0m",
+                true,
+            ],
+            [
+                '<blackbg>blackbg text</blackbg>',
+                "{$esc}[40mblackbg text{$esc}[0m",
+                true,
+            ],
+            [
+                '<redbg>redbg text</redbg>',
+                "{$esc}[41mredbg text{$esc}[0m",
+                true,
+            ],
+            [
+                '<greenbg>greenbg text</greenbg>',
+                "{$esc}[42mgreenbg text{$esc}[0m",
+                true,
+            ],
+            [
+                '<yellowbg>yellowbg text</yellowbg>',
+                "{$esc}[43myellowbg text{$esc}[0m",
+                true,
+            ],
+            [
+                '<bluebg>bluebg text</bluebg>',
+                "{$esc}[44mbluebg text{$esc}[0m",
+                true,
+            ],
+            [
+                '<magentabg>magentabg text</magentabg>',
+                "{$esc}[45mmagentabg text{$esc}[0m",
+                true,
+            ],
+            [
+                '<cyanbg>cyanbg text</cyanbg>',
+                "{$esc}[46mcyanbg text{$esc}[0m",
+                true,
+            ],
+            [
+                '<whitebg>whitebg text</whitebg>',
+                "{$esc}[47mwhitebg text{$esc}[0m",
+                true,
+            ],
+            [
+                '<strong>bold text</strong>',
+                'bold text',
+                false,
+            ],
+        ];
+    }
+}
